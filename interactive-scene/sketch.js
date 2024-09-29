6e452bbc4927d7d5133bc182cc700f68de5943b3
TopBunkBandit <jamesamitchell07@gmail.com>
2024-09-29 11:05:29 -0600

    good enough (random movement wasnt working so it will now change speed on rebound)

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -2,8 +2,9 @@
 // James Mitchell
 // 25/9/2024
 // a little dodging game(?) avoid the other ball if you want, doesnt really matter
+// controls are WASD for movement, double click restarts the game and scroll wheel changes your speed
 // Extra for Experts:
-// - made a basic addition of allowing the mouse wheel to change the speed at which your ball moves
+// - mousewheel changes speed, changing the shape of the window will automatically reset the game as to prevent any issues with the borders, dunno if that counts for anything
 
 
 let speed = 1;
@@ -11,23 +12,36 @@ let radius = 25;
 let enemyRadius = 25;
 let y = 200;
 let x = 200;
-let eX = radius;
-let eY = radius;
+let eX = radius + 5;
+let eY = radius + 5;
 let dx = 4;
 let dy = 2.5;
 let borderUpY = false;
 let borderDownY = false;
 let borderRightX = false;
 let borderLeftX = false;
+let borderEnemyUpY = false;
+let borderEnemyDownY = false;
+let borderEnemyRightX = false;
+let borderEnemyLeftX = false;
 let playerPosition = (x,y);
 let changePlayerColor = false;
 let lastTimeBounced = 0;
 let gameOver = false;
+let i = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  eX = radius + 5
+  eY = radius + 5
+  speed = 1
+  x = windowWidth/2 - radius
+  y = windowHeight/2 - radius
+}
 
 function draw() {
   background("lightblue");
@@ -61,117 +75,11 @@ function moveTime(){
   if (keyIsDown(68) && borderRightX === false){
     x += speed;
   } 
-  if (!changePlayerColor){
-    circle(x, y, radius*2);
-  }
-  else{
-    fill(random(255), random(255), random(255));
-    circle(x, y, radius*2);
-    changePlayerColor = false;
-  }
-}
-
-
-function checkForBorderX(){
-  if (x < radius){
-    borderLeftX = true;
-  }
-  else{
-    borderLeftX = false;
-  }
-  
-  if (x > width - radius){ds
-    borderRightX = true;
-  }  
-  else{
-    borderRightX = false;
-  }
-}
-
-function checkForBorderY(){
-  if (y < radius){
-    borderUpY = true;
-  }
-  else{
-    borderUpY = false;
-  }
-  
-  if (y > height - radius){
-    borderDownY = true;
-  }  
-  else{
-    borderDownY = false;
-  }
-}
-
-
-function mousePressed() {
-  if (mouseX <= eX + 10 && mouseX >= eY - 10){
-    enemyRadius = random(10, 75);
-
-  }
-}
-
-
-function mouseWheel(event) {
-  if (event.delta < 0 && speed <= 25){
-    speed += 0.1;
-  }
-  else if (event.delta > 0 && speed >= 0){
-    speed -= 0.1;
-
-  }
-}
-
-function doubleClicked(){
-  gameOver = false;
+  fill('green')
+  circle(x, y, radius*2);
 
 }
 
-function enemyMovement(){  
-  moveEnemy();
-  bounceEnemy();
-  displayEnemy();
-
-}
-
-function moveEnemy(){
-  eX = eX + dx;
-  eY = eY + dy;
-
-}
-
-// function bounceEnemy(){
-//   if (eX >= width - radius || eX <= 0 + radius) {
-//     dx = dx * -1;
-//   }
-  
-//   if (eY >= height - radius || eY <= 0 + radius) {
-//     dy = dy * -1;
-//   }
-// }
-
-function bounceEnemy(){
-  if (eX >= random(0, 400) && millis() >= lastTimeBounced ) {
-    dx = dx * -1;
-    lastTimeBounced = millis() + 500 
-  }
-  
-  if (eY >= random(0, 400) && millis() >= lastTimeBounced) {
-    dy = dy * -1;
-    lastTimeBounced = millis() + 500 
-  }
-}
-
-
-function displayEnemy(){
-  if (gameOver === false){
-    circle(eX, eY, radius*2);
-  }
-  else{
-    fill(255);
-  }
-}
 
 function checkForBorderX(){
   if (x < radius){
@@ -202,6 +110,122 @@ function checkForBorderY(){
   }  
   else{
     borderDownY = false;
+  }
+}
+
+
+
+function mouseWheel(event) {
+  if (event.delta < 0 && speed <= 10){
+    speed += 0.1;
+  }
+  else if (event.delta > 0 && speed >= 0){
+    speed -= 0.1;
+
+  }
+}
+
+function doubleClicked(){
+  gameOver = false;
+  eX = radius + 5
+  eY = radius + 5
+  speed = 1
+  x = windowWidth/2 - radius
+  y = windowHeight/2 - radius
+
+}
+
+function enemyMovement(){  
+  moveEnemy();
+  bounceEnemy();
+  displayEnemy();
+
+}
+
+function moveEnemy(){
+  eX = eX + dx;
+  eY = eY + dy;
+
+}
+
+function bounceEnemy(){
+  if (eX >= width - radius || eX <= 0 + radius) {
+    rotate(random(0, 90))
+    dx = dx * random(-2, -0.5);
+  }
+  
+  if (eY >= height - radius || eY <= 0 + radius) {
+    dy = dy * random(-2, -0.5);
+  }
+}
+
+// function bounceEnemy(){
+//   if (!borderEnemyLeftX && !borderEnemyRightX){
+//     if (millis() >= lastTimeBounced ) {
+//       dx = dx * -1;
+//       lastTimeBounced = millis() + random(1000, 5000) 
+//     }
+//   else{
+//     dx = dx * -1;
+//     for (let i = 0; i <= 5; i++) {
+//      dx += 1 
+//     }
+//     i = 0
+//     }
+// }
+//   if (!borderEnemyDownY && !borderEnemyUpY)
+//     if (millis() >= lastTimeBounced) {
+//       dy = dy * -1;
+//       lastTimeBounced = millis() + random(1000, 5000)  
+//     }
+//     else{
+//       dy = dy * -1;
+//       for (let i = 0; i <= 5; i++) {
+//        dy += 1 
+//       }
+//     }
+// }
+
+
+function displayEnemy(){
+  if (gameOver === false){
+    fill('redd')
+    circle(eX, eY, radius*2);
+  }
+  else{
+    fill(255);
+  }
+}
+
+function checkForEnemyBorderX(){
+  if (eX < radius){
+    borderEnemyLeftX = true;
+  }
+  else{
+    borderEnemyLeftX = false;
+  }
+  
+  if (eX > width - radius){
+    borderEnemyRightX = true;
+  }  
+  else{
+    borderEnemyRightX = false;
+  }
+}
+
+function checkForEnemyBorderY(){
+  if (eY < radius){
+    borderEnemyUpY = true;
+  }
+  else{
+    borderEnemyUpY = false;
+  }
+  
+  if (eY > height - radius){
+    borderEnemyDownY = true;
+  }  
+  else{
+    borderEnemyDownY = false;
   }
 }
 
