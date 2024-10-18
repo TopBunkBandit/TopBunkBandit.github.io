a9ae52534f51d95567f5eba375f7ff12a8ff8798
TopBunkBandit <jamesamitchell07@gmail.com>
2024-10-18 14:15:15 -0600

    Added a working player and a way to shoot
    need to work on the balls keeping their original launch angle no matter what and need to add hitboxes for the ships, after that we shall see what happens

--- array-and-object-assignment/sketch.js
+++ array-and-object-assignment/sketch.js
@@ -2,11 +2,10 @@
 // James Mitchell
 // 9/10/24
 // current goals:
-// add the little person at the bottom to destroy the ships
+// change the balls to fire at a angle and keep that angle reguardless of what the rotateRect changes to
 // add ability for ships to drop bombs after they are destroyed, needs to be implemented along with player movement along the X axis
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// as far as im aware nothing
 
 let theShips = [];
 let deathLocation = [];
@@ -20,13 +19,17 @@ let goDown = false;
 let lastTimeMoved = 0;
 let centerX;
 let rotateRect = 0;
+let circleCenter;
+let rectCenterX;
+let rectCenterY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   spawnships();
   centerX = width/2;
+  circleCenter = height - 75;
   angleMode(DEGREES);
-  rectMode(CENTER);
+  // rectMode(CENTER);
 
   window.setInterval(spawnships, 3000);
 }
@@ -40,6 +43,7 @@ function draw() {
   playerBase();  
   movePlayerProjectiles();
   showPlayerProjectile();
+  killOffEntitys();
 }
 
 //makes the ships move in more blocky movements, allowing the player to have time to react
@@ -99,51 +103,81 @@ function playerBase(){
 
 //creates the "weapon" that the player will use, it rotates to match the direction the player wants it to
 function playerRect(){
-  if (keyIsDown(LEFT_ARROW)){
-    rotateRect += 1;
+  if (keyIsDown(LEFT_ARROW) && rotateRect > -22){
+    rotateRect -= 0.5;
   }
-  if (keyIsDown(RIGHT_ARROW)){
-    rotateRect -= 1;
+  if (keyIsDown(RIGHT_ARROW) && rotateRect < 16){
+    rotateRect += 0.5;
   }
   stroke(1);
   fill("white");
   push();
-  translate(250, 700);
+  rectMode(CENTER);
+  translate(centerX - 25, circleCenter);
   rotate(rotateRect);
-  rect(-5, 25, 50, 100);
+  rectCenterX = 20;
+  rectCenterY = 25;
+  rect(20, 25, 50, 100);
   pop();
 }
 
+
+
+//working on creating the projectiles
 function keyPressed(){
   if (keyIsDown(32)){
     let projectile = {
-      x: 10,
-      y: 10,
-      radius: 25,
-      speedX: 10,
-      speedY: 15,
+      ax: width/2,
+      ay: height - 100,
+      speedX: 1,
+      speedY: 1,
     };
     playerProjectiles.push(projectile);
   }
 }
 
+//get the balls to move with a X increase based off of rotateRect at the time that the ball is launched
+//currently running into issues with the balls keeping the rotateRect and moving with the rect
+// they should be moving in that direction and not change direction no matter what
 function movePlayerProjectiles(){
   for (projectiles of playerProjectiles){
-    projectiles.x += projectiles.speedX;
-    projectiles.y += projectiles.speedY;
-
+    projectiles.ay -= projectiles.speedY;
+    projectiles.ax += rotateRect/50;
+    
   }
 }
 
+
+//Displays the players projectiles
 function showPlayerProjectile(){
-  for (let projectile of theShips){
+  for (let projectile of playerProjectiles){
     noStroke;
-    fill("dark green");
-    circle(projectile.x, projectile.y, projectile.radius);
+    fill("purple");
+    circle(projectile.ax, projectile.ay, 25);
   }
+}
+
+
+//removes any ships or balls that leave the screen
+//might need to remove the ships one in favor of a loss screen IDK tho
+function killOffEntitys(){
+  for (ship of theShips){
+    if (ship.y > height){
+      let theIndex = theShips.indexOf(ship);
+      theShips.splice(theIndex, 1);
+    }
+  }
+  for (projectile of playerProjectiles){
+    if (projectile.ay < -15 || projectile.ax < -15 || projectile.ax > windowWidth + 15){
+      let theIndex = playerProjectiles.indexOf(projectile);
+      playerProjectiles.splice(theIndex, 1);
+    }
+  }
+
 }
 
 // not working on it yet but this is the shell code for the enemy destruction
+// make sure that you check for all rects on all balls, else it may cause issues
 
 // function dropBombs(){
 //   for (let bomb of deathLocation){
